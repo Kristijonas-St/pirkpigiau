28723af68dc47272947c1898e3cdb27a86f2f0f8
Kristijonas_St <kristijonasstanelis@gmail.com>
2025-03-13 19:37:46 +0200

    add a formatted text-to-speech response of scraping results

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import sounddevice as sd
+import tempfile
+import os
+import time
+import subprocess
+import gtts
+
 from scraping_feature.scraping_feature import ScrapingRequest
 
 maxima_url = "https://www.maxima.lt/pasiulymai"
@@ -6,6 +13,29 @@ fixed_item_url = "https://barbora.lt/produktai/citrinos-3-4-d-1-kg"
 item_to_search = "Citrinos"
 
 st.title("Maxima.lt pigiausios prekės")
+
+def speak(text, lang="lt"):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+        filename = temp_file.name
+
+    tts = gtts.gTTS(text, lang=lang)
+    tts.save(filename)
+
+    if os.name == "nt":
+        os.startfile(filename)
+    else:
+        subprocess.Popen(["mpg321", filename])
+
+    time.sleep(5)
+
+    for _ in range(3):
+        try:
+            os.remove(filename)
+            break
+        except PermissionError:
+            time.sleep(2)
+
+
 
 if st.button("Pasakykite prekę"):
     request = ScrapingRequest("Maxima", maxima_url, item_to_search).scrape_price()
@@ -15,5 +45,10 @@ if st.button("Pasakykite prekę"):
     elif request.cheapest_item:
         name, price = request.cheapest_item
         st.markdown(f"Pigiausias variantas: [{name}]({fixed_item_url}) už {price}", unsafe_allow_html=True)
+        
+        price_value = float(price.replace(" €", "").replace(",", "."))
+        speech_response = f"{int(price_value)} euru {round((price_value % 1) * 100)} centai"
+
+        speak(speech_response)
     else:
         st.write("Nepavyko rasti prekės")
