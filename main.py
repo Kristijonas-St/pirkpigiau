7ec9ada6256d374f58456528bbb22a34772d6f50
Kristijonas_St <kristijonasstanelis@gmail.com>
2025-03-13 19:56:41 +0200

    add case formatting for euros and cents

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ item_to_search = "Citrinos"
 
 st.title("Maxima.lt pigiausios prekės")
 
+def form_euro_and_cent_case(price_value):
+    euro = int(price_value)
+    if euro == 1 or str(euro)[-1] == "1":
+        euro_case = "euras"
+    elif euro == 0 or str(euro)[-1] == "0":
+        euro_case = "eurų"
+    else:
+        euro_case = "eurai"
+
+    cents = round((price_value % 1) * 100)
+    if cents == 1 or str(cents)[-1] == "1":
+        cent_case = "centas"
+    elif cents == 0 or str(cents)[-1] == "0":
+        cent_case = "centų"
+    else:
+        cent_case = "centai"
+
+    return euro_case, cent_case
+
 def speak(text, lang="lt"):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
         filename = temp_file.name
@@ -27,14 +46,12 @@ def speak(text, lang="lt"):
         subprocess.Popen(["mpg321", filename])
 
     time.sleep(5)
-
     for _ in range(3):
         try:
             os.remove(filename)
             break
         except PermissionError:
             time.sleep(2)
-
 
 
 if st.button("Pasakykite prekę"):
@@ -47,8 +64,12 @@ if st.button("Pasakykite prekę"):
         st.markdown(f"Pigiausias variantas: [{name}]({fixed_item_url}) už {price}", unsafe_allow_html=True)
         
         price_value = float(price.replace(" €", "").replace(",", "."))
-        speech_response = f"{int(price_value)} euru {round((price_value % 1) * 100)} centai"
-
+        euro_case, cent_case = form_euro_and_cent_case(price_value)
+        speech_response = f"Preke, {item_to_search}, pigiausia, Maximoje, {int(price_value)}, {euro_case}, {round((price_value % 1) * 100)}, {cent_case}"
         speak(speech_response)
+
     else:
         st.write("Nepavyko rasti prekės")
+
+
+
