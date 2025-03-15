1aec98dcd6d28f36c3f126e093405a000c63ab9d
Kristijonas_St <kristijonasstanelis@gmail.com>
2025-03-15 12:57:25 +0200

    add scraping for 'Bananai' in RIMI

--- scraping_feature/scraping_feature.py
+++ scraping_feature/scraping_feature.py
@@ -11,57 +11,46 @@ class ScrapingRequest:
         self.error_message = None
         # TODO: Add functionality to also return `item_url` from this class
 
-    
     def scrape_price(self):
         headers = {'User-Agent': 'Mozilla/5.0'}
 
+        match self.shop_name:
+            case "Maxima":
+                pass
+            case "IKI":
+                pass            
+            case "Rimi":
+                self.scrape_rimi(headers)
+            case _:
+                print("Unknown error")
+
+    def scrape_rimi(self, headers):
         response = requests.get(self.shop_url, headers=headers)
         if response.status_code != 200:
             self.error_message = f"Nepavyko pasiekti {self.shop_name}  pasiūlymų puslapio."
+            print(self.error_message)
             return self
 
         soup = BeautifulSoup(response.text, 'html.parser')
-        products_containers = soup.find_all('div', class_='card card-small is-pointer h-100')
 
-        products = []
-        products_prices = []
+        products = soup.find_all('li', class_='product-grid__item')
+        for product in products:
+            if self.item in product.text:
+                #print(product.text.replace('\n','').strip())
+                print(f"Found {self.item}: {product.text.replace('\n','').strip()}")
 
-        for product_container in products_containers:
-            if product_container.find('div', class_='bg-primary text-white h-100 rounded-end-1'):
-                products.append(product_container.find('div', class_='w-100 mb-auto'))
-                products_prices.append(product_container.find('div', class_='d-flex justify-content-center'))
+scrape = ScrapingRequest('Rimi', 'https://www.rimi.lt/e-parduotuve/lt/produktai/vaisiai-darzoves-ir-geles/c/SH-15', 'Bananai')
+scrape.scrape_price()
 
-        mapping = dict(zip(products, products_prices))
-        found_items = []
 
-        for product_name in mapping:
-            title_element = product_name.find('h4', class_='mt-4 text-truncate text-truncate--2')
 
-            if title_element:
-                title = title_element.text.strip().lower()
 
-                if fuzz.partial_ratio(self.item.lower(), title) > 80:
-                    price_whole_element = mapping[product_name].find('div', class_='my-auto price-eur text-end')
+        
 
-                    temp_div = mapping[product_name].find('div', class_='d-flex flex-column my-auto')
+        
 
-                    price_decimal_element = temp_div.find('span', class_='price-cents')
+        
 
-                    price_whole = price_whole_element.text.strip()
-                    price_decimal = price_decimal_element.text.strip()
-
-                    price = f"{price_whole}.{price_decimal} €"
-
-                    found_items.append((title, price))
-
-        if not found_items:
-            self.error_message = f"Prekės \"{self.item}\" Maximos pasiūlymuose nerasta."
-            return self
-
-        found_items.sort(key=lambda x: float(x[1].replace(" €", "").replace(",", ".")))
-        self.cheapest_item = found_items[0]
-
-        return self
 
 
 
