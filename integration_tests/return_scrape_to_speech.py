bd3c828dd2e32e83f6dd84c1b9d7e0a9e6517504
Kristijonas_St <kristijonasstanelis@gmail.com>
2025-04-05 15:54:28 +0300

    modify return_scrape_to_speech.py integration test so it checks functional and nonfunctional requirements

--- integration_tests/return_scrape_to_speech.py
+++ integration_tests/return_scrape_to_speech.py
@@ -1,33 +1,25 @@
-
-import pytest
-from unittest.mock import patch
-
+import time
 from scraping_feature.scraping_feature import ScrapingRequest
 from speech_response_feature.speech_response import say_formatted_response
 
-@pytest.mark.integration
-@patch("speech_response_feature.speech_response.speak")
-def test_rimi_scraped_speech_response(mock_speak):
-
+def test_rimi_scraped_speech_response():
     item_name = "pomidorai"
     shop_name = "Rimi"
     scraper = ScrapingRequest(shop_name, item_name)
 
     result = scraper.scrape_price()
-    
     assert result is not None, f"{item_name} not found in {shop_name}"
 
-    print(f"\nScraped item: {result.item_name}")
-
+    start_time = time.time()
     say_formatted_response(
         is_found=True,
         item_to_search=result.item_name,
         shop_name=shop_name,
         price=result.cheapest_item
     )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-    assert mock_speak.called, "speak() was not called"
-    
-    spoken_text = mock_speak.call_args[0][0]
-    assert item_name[:-3] in spoken_text.lower(), f"Response does not contain {item_name}"
-    assert shop_name in spoken_text, f"Response doesn't contain {shop_name}"
+    print(f"Speech response time: {elapsed_time:.2f} seconds")
+    assert elapsed_time >= 4, f"Response time was {elapsed_time:.2f} - TOO QUICK"
+
